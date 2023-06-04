f358d38a37f1c73a28daa95f7c3afc1d38cda208
dmitry-indenbom <dmitry.indenbom@yandex.ru>
2023-06-05 01:37:42 +0300

    Обновлены выводы для обезличивания

--- deid_train.py
+++ deid_train.py
@@ -38,12 +38,14 @@ if __name__ == '__main__':
                                     is_uncased=data_config["is_uncased"],
                                     pretrained_tokenizer=data_config["pretrained_tokenizer_path"],
                                     max_length=data_config["max_token_number"],
+                                    overlap=data_config["overlap"],
                                     eq_max_padding=data_config["eq_max_padding"])
     val_dataset = get_ner_dataset(data_type=data_config["val_data_type"],
                                   path_to_folder=data_config["validate_data_path"],
                                   is_uncased=data_config["is_uncased"],
                                   pretrained_tokenizer=data_config["pretrained_tokenizer_path"],
                                   max_length=data_config["max_token_number"],
+                                  overlap=data_config["overlap"],
                                   eq_max_padding=data_config["eq_max_padding"])
     train_dataloader = DataLoader(train_dataset, shuffle=True,
                                   batch_size=data_config["batch_size"],
